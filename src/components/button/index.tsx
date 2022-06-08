8dca4e2273d51e0019b41896ede1aa7f5c63aac4
Oh Seung Hyeok <brown8167@gmail.com>
2022-06-09 07:14:05 +0900

    feat: 버튼 컴포넌트 추가

---
+++ src/components/button/index.tsx
@@ -0,0 +1,42 @@
+import { FC, ReactNode } from 'react'
+import styled from 'styled-components'
+import { Colors } from '../../styles/theme'
+
+interface ButtonProps extends StyleProps {
+    children: ReactNode
+    onClick: () => void
+}
+
+interface StyleProps {
+    width?: string
+    BgColor?: boolean
+}
+
+const Container = styled.button<StyleProps>`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 8px;
+    padding: 8px 16px;
+    border: solid 1px ${Colors.black[900]};
+    ${({ BgColor }) => BgColor ?
+        `background-color: ${Colors.black[900]}; color: ${Colors.white[900]};`
+        : `background-color: transparent; color: ${Colors.black[900]};`}
+    cursor: pointer;
+    transition: background-color .3s, color .3s;
+    &:hover {
+        ${({ BgColor }) => BgColor ?
+        `background-color: transparent; color: ${Colors.black[900]};`
+        : `background-color: ${Colors.black[900]}; color: ${Colors.white[900]};`}
+    }
+`
+
+const Button: FC<ButtonProps> = ({ children, width, BgColor = false, onClick }) => {
+    return (
+        <Container width={width} BgColor={BgColor} onClick={onClick}>
+            {children}
+        </Container>
+    )
+}
+
+export default Button
