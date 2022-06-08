d6d43f39ba7b4a7f3602af0f404e55fc9e6c00ad
Oh Seung Hyeok <brown8167@gmail.com>
2022-06-09 07:16:40 +0900

    fix: globalStyle 수정

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -54,19 +54,31 @@ export const GlobalStyle = createGlobalStyle`
         url('/fonts/NotoSans-Black.otf') format('opentype');
     }
     
-    * {
+    html, body {
+        padding: 0;
+        margin: 0;
         font-family: 'Noto Sans KR', sans-serif;
     }
 
-    body, html {
-        background: #fbfbff;
+    * {
+        box-sizing: border-box;
     }
 
-    body, p {
+    a {
+        color: inherit;
+        text-decoration: none;
+    }
+
+    body {
+        background: #f4f7fd;
+    }
+
+    p {
         margin: 0;
     }
 
     input, button {
+        padding: 0;
         outline: none;
-    }
+    }  
 `
