15e936ffb516b5e9eb5460e021106671a099fee1
Oh Seung Hyeok <brown8167@gmail.com>
2022-06-11 20:35:06 +0900

    fix: button 디자인 수정 및 컴포넌트 타입 수정

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,9 +1,9 @@
-import { FC, ReactNode } from 'react'
+import { NextPage } from 'next'
 import styled from 'styled-components'
 import { Colors } from '../../styles/theme'
 
 interface ButtonProps extends StyleProps {
-    children: ReactNode
+    children: React.ReactNode
     onClick: () => void
 }
 
@@ -18,20 +18,20 @@ const Container = styled.button<StyleProps>`
     align-items: center;
     border-radius: 8px;
     padding: 8px 16px;
-    border: solid 1px ${Colors.black[900]};
+    border: solid 1px ${Colors.black[700]};
     ${({ BgColor }) => BgColor ?
-        `background-color: ${Colors.black[900]}; color: ${Colors.white[900]};`
+        `background-color: ${Colors.black[800]}; color: ${Colors.white[900]};`
         : `background-color: transparent; color: ${Colors.black[900]};`}
     cursor: pointer;
     transition: background-color .3s, color .3s;
     &:hover {
         ${({ BgColor }) => BgColor ?
-        `background-color: transparent; color: ${Colors.black[900]};`
+        `background-color: ${Colors.black[900]};`
         : `background-color: ${Colors.black[900]}; color: ${Colors.white[900]};`}
     }
 `
 
-const Button: FC<ButtonProps> = ({ children, width, BgColor = false, onClick }) => {
+const Button: NextPage<ButtonProps> = ({ children, width, BgColor = false, onClick }) => {
     return (
         <Container width={width} BgColor={BgColor} onClick={onClick}>
             {children}
