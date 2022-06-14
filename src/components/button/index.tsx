72e236ff71573ef30a7fe0dd1bacf5f55dfec513
Oh Seung Hyeok <brown8167@gmail.com>
2022-06-15 07:18:04 +0900

    feat: 모바일 화면일 때 button display 조정

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from 'next'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { media } from '../../styles/media'
 import { Colors } from '../../styles/theme'
 
 interface ButtonProps extends StyleProps {
@@ -10,30 +11,34 @@ interface ButtonProps extends StyleProps {
 interface StyleProps {
     width?: string
     BgColor?: boolean
+    mobileDisplay?: string
 }
 
 const Container = styled.button<StyleProps>`
-    display: flex;
+    display: ${({mobileDisplay}) => mobileDisplay};
     justify-content: center;
     align-items: center;
     border-radius: 8px;
     padding: 8px 16px;
     border: solid 1px ${Colors.black[700]};
+    ${media.mobile(css`
+        display: flex;
+    `)}
     ${({ BgColor }) => BgColor ?
-        `background-color: ${Colors.black[800]}; color: ${Colors.white[900]};`
+        `background-color: ${Colors.black[800]}; color: ${Colors.white};`
         : `background-color: transparent; color: ${Colors.black[900]};`}
     cursor: pointer;
     transition: background-color .3s, color .3s;
     &:hover {
         ${({ BgColor }) => BgColor ?
         `background-color: ${Colors.black[900]};`
-        : `background-color: ${Colors.black[900]}; color: ${Colors.white[900]};`}
+        : `background-color: ${Colors.black[900]}; color: ${Colors.white};`}
     }
 `
 
-const Button: NextPage<ButtonProps> = ({ children, width, BgColor = false, onClick }) => {
+const Button: NextPage<ButtonProps> = ({ children, width, BgColor = false, onClick, mobileDisplay = 'flex' }) => {
     return (
-        <Container width={width} BgColor={BgColor} onClick={onClick}>
+        <Container width={width} BgColor={BgColor} onClick={onClick} mobileDisplay={mobileDisplay}>
             {children}
         </Container>
     )
