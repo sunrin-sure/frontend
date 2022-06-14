a788c327f2f3c3b213929251bdde6f040fe9ce94
Oh Seung Hyeok <brown8167@gmail.com>
2022-06-15 07:37:59 +0900

    feat: 메인페이지 디자인 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,57 +1,31 @@
 import type { NextPage } from 'next'
-import styled, { css } from 'styled-components'
-import { FontSizes, Colors } from '../styles/theme'
-import { media } from '../styles/media'
-import Counter from '../components/Counter'
 
-import Button from '../components/button'
+// Component
 import Layout from '../components/layout'
-
-const Title = styled.h1`
-  ${FontSizes.display1}
-  color: ${Colors.red[600]};
-
-  ${media.mobile(css`
-    color: ${Colors.blue[600]};
-    ${FontSizes.heading1}
-  `)}
-
-  ${media.tabletM(css`
-    color: ${Colors.black[300]};
-    ${FontSizes.heading3}
-  `)}
-
-  ${media.tabletL(css`
-    color: ${Colors.blue[300]};
-    ${FontSizes.text}
-  `)}
-
-  ${media.desktopS(css`
-    color: ${Colors.black[900]};
-    ${FontSizes.strong}
-  `)}
-
-  ${media.desktopM(css`
-    color: ${Colors.red[300]};
-    ${FontSizes.info}
-  `)}
-  
-  ${media.desktopL(css`
-    color: ${Colors.white[900]};
-    ${FontSizes.small}
-  `)}
-`
+import Inner from '../components/layout/Inner'
+import MainSection from '../components/page/MainPageSection'
+import MemberSmallBox from '../components/post/MemberSmallBox'
+import ProjectBox from '../components/post/ProjectBox'
 
 const IndexPage: NextPage = () => {
-  return (
-    <Layout>
-      <p>hello</p>
-      <Title>Title</Title>
-      <Counter />
-      <hr />
-      <Button onClick={() => console.log("click")}>버튼</Button>
-    </Layout>
-  )
+	const List = [
+		{ value: 'all', name: '직무' },
+		{ value: 'planner', name: '기획자' },
+		{ value: 'designer', name: '디자이너' },
+		{ value: 'frontend', name: '프론트엔드 개발자' },
+		{ value: 'backend', name: '백엔드 개발자' },
+	]
+
+	return (
+		<Layout title='메인페이지'>
+			<Inner>
+				<MainSection
+					projectComponent={List.map((item) => <ProjectBox key={item.value} projectId={''} />)}
+					memberComponent={List.map((item) => <MemberSmallBox key={item.value} />)}
+				/>
+			</Inner>
+		</Layout>
+	)
 }
 
 export default IndexPage
