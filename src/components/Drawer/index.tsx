b4468ce33912797f0b44c15869537eef0f9e8065
Oh Seung Hyeok <brown8167@gmail.com>
2022-06-17 21:27:28 +0900

    fix: import React 삭제

--- src/components/Drawer/index.tsx
+++ src/components/Drawer/index.tsx
@@ -1,6 +1,5 @@
 import { useRouter } from 'next/router'
 import Link from 'next/link'
-import React from 'react'
 
 import styled from 'styled-components'
 import { Colors } from '../../styles/theme'
