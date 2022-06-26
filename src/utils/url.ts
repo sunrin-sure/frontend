c7cb2932038b6e9433ae416a175dc3134eba1114
Oh Seung Hyeok <brown8167@gmail.com>
2022-06-26 18:27:16 +0900

    fix: 개발 서버 도메인 변경

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : 'https://api.rarp.kr'
+export const BASE_URL = process.env.NODE_ENV === 'development' ? 'https://sunrin-sure.herokuapp.com' : 'https://api.rarp.kr'
 
 export const AUTH_API = `${BASE_URL}/auth`
 export const USERS_API = `${BASE_URL}/users`
