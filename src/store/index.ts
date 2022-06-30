ff65af23ab94ac039dc83ea08cecd6e8d5627268
Oh Seung Hyeok <brown8167@gmail.com>
2022-07-01 03:14:34 +0900

    feat: sagaStore interface 추가

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,12 @@
-import { createStore, compose, applyMiddleware } from 'redux'
+import { createStore, compose, applyMiddleware, AnyAction, Store } from 'redux'
 import { createWrapper } from 'next-redux-wrapper'
-import createSagaMiddleware from 'redux-saga'
+import createSagaMiddleware, { Task } from 'redux-saga'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import logger from 'redux-logger'
 
 import rootReducer from './reducers'
 import rootSaga from './sagas'
+import { rootState } from './interface/state.interface'
 
 const devMode = process.env.NODE_ENV === 'development'
 
@@ -17,8 +18,12 @@ const configureStore = () => {
             : compose(applyMiddleware(sagaMiddleware))
     const store = createStore(rootReducer, enhancer);
 
-    (store as any).sagaTask = sagaMiddleware.run(rootSaga)
+    (store as SagaStore).sagaTask = sagaMiddleware.run(rootSaga)
     return store
+}
+
+export interface SagaStore extends Store<rootState, AnyAction> {
+    sagaTask: Task
 }
 
 export default createWrapper(configureStore, { debug: devMode })
