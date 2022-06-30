1087592712083386a76b0fac9cbff0a71a0bea36
Oh Seung Hyeok <brown8167@gmail.com>
2022-07-01 03:15:43 +0900

    feat: SS 데이터를 CS store에 추가

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,5 +1,19 @@
-import { combineReducers } from 'redux'
-
+import { AnyAction, CombinedState, combineReducers } from 'redux'
+import { HYDRATE } from "next-redux-wrapper"
 import auth from './authReducer'
+import { rootState } from '../interface/state.interface'
 
-export default combineReducers({ auth })
+const rootReducer = (state: rootState | undefined , action: AnyAction):CombinedState<rootState> => {
+    switch (action.type) {
+        case HYDRATE:
+            return { ...action.payload }
+        default: {
+            const combineReducer = combineReducers({
+                auth
+            })
+            return combineReducer(state, action)
+        }
+    }
+}
+
+export default rootReducer
