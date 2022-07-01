9a36abf0759d7e138ad4fe8560d94d7a1d717144
Oh Seung Hyeok <brown8167@gmail.com>
2022-07-01 18:18:49 +0900

    fix: rewrite 삭제

--- next.config.js
+++ next.config.js
@@ -2,17 +2,8 @@
 const nextConfig = {
     reactStrictMode: false,
     compiler: {
-      styledComponents: true,
+        styledComponents: true,
     },
-    async rewrites() {
-      const BASE_URL = process.env.NODE_ENV === 'development' ? 'https://sunrin-sure.herokuapp.com' : 'https://api.rarp.kr'
-      return [
-        {
-          source: '/:path*',
-          destination: `${BASE_URL}/:path*`,
-        },
-      ];
-    },
-  }
-  
-  module.exports = nextConfig
+};
+
+module.exports = nextConfig;
