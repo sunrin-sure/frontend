f036db18c301eba59839f0ab6baa3dcc07d7505b
Oh Seung Hyeok <brown8167@gmail.com>
2022-07-02 01:38:23 +0900

    fix: 개발 api url 설정

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,4 +1,5 @@
-export const BASE_URL = process.env.NODE_ENV === 'development' ? 'https://sunrin-sure.herokuapp.com' : 'https://api.rarp.kr'
+// export const BASE_URL = process.env.NODE_ENV === 'development' ? 'https://sunrin-sure.herokuapp.com' : 'https://api.rarp.kr'
+export const BASE_URL = 'https://sunrin-sure.herokuapp.com'
 
 export const AUTH_API = `${BASE_URL}/auth`
 export const USERS_API = `${BASE_URL}/users`
