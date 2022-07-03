0938f828a3301019cd778f28d721a2354ecf8eb1
Oh Seung Hyeok <brown8167@gmail.com>
2022-07-04 00:43:08 +0900

    fix: SagaStore 타입 추가

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,4 @@
-import { createStore, compose, applyMiddleware, AnyAction, Store } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
 import { createWrapper } from 'next-redux-wrapper'
 import createSagaMiddleware, { Task } from 'redux-saga'
 import { composeWithDevTools } from 'redux-devtools-extension'
@@ -6,24 +6,25 @@ import logger from 'redux-logger'
 
 import rootReducer from './reducers'
 import rootSaga from './sagas'
-import { rootState } from './interface/state.interface'
+
+declare module 'redux' {
+    export interface Store {
+        sagaTask?: Task
+    }
+}
 
 const devMode = process.env.NODE_ENV === 'development'
 
 const configureStore = () => {
     const sagaMiddleware = createSagaMiddleware()
     const enhancer =
-            devMode
+        devMode
             ? composeWithDevTools(applyMiddleware(logger, sagaMiddleware))
             : compose(applyMiddleware(sagaMiddleware))
-    const store = createStore(rootReducer, enhancer);
+    const store = createStore(rootReducer, enhancer)
 
-    (store as SagaStore).sagaTask = sagaMiddleware.run(rootSaga)
+    store.sagaTask = sagaMiddleware.run(rootSaga)
     return store
-}
-
-export interface SagaStore extends Store<rootState, AnyAction> {
-    sagaTask: Task
 }
 
 export default createWrapper(configureStore, { debug: devMode })
